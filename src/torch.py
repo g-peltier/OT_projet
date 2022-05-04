7d08fb37758e4c4c0d84536c17a3f1ea3eb288b6
Vladimir Kondratyev <kondratyev.w.i@gmail.com>
2022-05-04 14:28:52 +0200

    Structure the code a little bit

---
+++ src/torch.py
@@ -0,0 +1,30 @@
+import torch
+
+class Sinkhorn():
+    
+    def __init__(self, eps, device=torch.device("cpu")):
+        self.eps = eps
+        self.device = device
+        
+    def forward(self, a, b, C, n_iter=100):
+        K = torch.exp(-C/self.eps)
+        v = torch.ones(b.shape[0], device=self.device)
+        for _ in range(n_iter):
+            u = a / (K@v)
+            v = b / (K.T@u)
+            
+        return ((torch.diag(u)@K)@torch.diag(v))
+        
+    def __call__(self, a, b, C, n_iter=100):
+        return self.forward(a, b, C, n_iter)
+    
+class WeightClipper(object):
+
+    def __init__(self, c=0.01):
+        self.c = c
+
+    def __call__(self, module):
+        if hasattr(module, 'weight'):
+            w = module.weight.data
+            w = w.clamp(-self.c,self.c)
+            module.weight.data = w
